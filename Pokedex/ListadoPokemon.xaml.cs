aef1401971b3195e9a1746016e88fbb0b95763d2
Imsael <ismaelrobalino03@gmail.com>
2024-05-27 08:59:41 -0500

    Agregar archivos de proyecto.

---
+++ Pokedex/ListadoPokemon.xaml.cs
@@ -0,0 +1,23 @@
+using Pokedex.Models;
+using Pokedex.Servicios;
+
+namespace Pokedex;
+
+public partial class ListadoPokemon : ContentPage
+{
+	public List<PokemonInfo> listado_pokemones;
+
+	public ListadoPokemon()
+	{
+		InitializeComponent();
+		LoadData();
+	}
+
+	public async void LoadData() { 
+	
+		PokemonAPIs _poke_apis = new PokemonAPIs();
+		listado_pokemones = await _poke_apis.DevuelveListadoPokemones();
+		PokemonList.ItemsSource = listado_pokemones;
+
+	}
+}
---
+++ Pokedex/Servicios/PokemonAPIs.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Pokedex.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Servicios
+{
+    public class PokemonAPIs
+    {
+        public HttpClient _httpClient;
+        public PokemonAPIs() { 
+
+        _httpClient=new HttpClient();
+        
+        }
+        public async Task <List<PokemonInfo>> DevuelveListadoPokemones() {
+
+            string url = "https://pokeapi.co/api/v2/pokemon/?limit=40&offset=0";
+            string json = await _httpClient.GetStringAsync(url);
+
+           PokemonList resourse_pokemon = JsonConvert.DeserializeObject<PokemonList>(json); 
+            return resourse_pokemon.results;
+
+        }
+
+
+    }
+}
